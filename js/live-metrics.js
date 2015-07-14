b54a99372680bea2930ca7f2758465fb17ade13d
dao hodac <dao.hodac@gmail.com>
2015-07-14 13:09:17 +0200

    add sample live counter

---
+++ js/live-metrics.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+var getSampleCount = function() {
+	$.getJSON(
+		"https://shared.flightwatching.com/public/metrics/samples.json",
+		function( data ) {
+			for (var i = 0; i < 10; i++) {
+				setTimeout(function(val) {
+					console.log(val)
+					$(".odometer").html(val);
+				}, 6000*i, Math.floor(+data.prev+(+data.next-data.prev)*i/10));
+			}
+		});
+}
+
+$(document).ready(function(){
+	getSampleCount();
+	setInterval(getSampleCount, 60000);
+});
